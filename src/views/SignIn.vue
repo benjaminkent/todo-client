3dd5fe19fa17964257dd347c7786c799425b7e24
benjaminkent <benjamin.k.jehl@gmail.com>
2019-04-04 21:49:16 -0400

    added signin methods

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -18,7 +18,47 @@
 
 <script>
 export default {
-  name: 'signin'
+  name: 'signin',
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  created() {
+    this.checkSignedIn()
+  },
+  updated() {
+    this.checkSignedIn()
+  },
+  methods: {
+    signin() {
+      this.$http.plain.post('/signin', { email: this.email, password: this.password })
+        .then(response => this.signinSuccessful(response))
+        .catch(error => this.signinFailed(error))
+    },
+    signinSuccessful(response) {
+      if (!response.data.csrf) {
+        this.signinFailed(response)
+        return
+      }
+      localStorage.csrf = response.data.csrf
+      localStorage.signedIn = true
+      this.error = ''
+      this.$router.replace('/todos')
+    },
+    signinFailed(error) {
+      this.error = (error.response && error.response.data && error.response.data.error) || ''
+      delete localStorage.csrf
+      delete localStorage.signedIn
+    },
+    checkSignedIn() {
+      if (localStorage.signedIn) {
+        this.$router.replace('/todos')
+      }
+    }
+  }
 }
 </script>
 
